d43c3adb088b406272c3b6e3be61c695e5821ab7
Erjon Lazellari <192823428+elazellari@users.noreply.github.com>
2025-07-13 14:27:42 -0400

    added search bar and button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-async function getWeather() {
+//global variables
+async function getWeather(selectedCity) {
   const response = await fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/toronto?unitGroup=us&key=M8V47U6EQ5XQSTQBETS9C4UX3&contentType=json",
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${selectedCity}?unitGroup=us&key=M8V47U6EQ5XQSTQBETS9C4UX3&contentType=json`,
     { mode: "cors" }
   );
   const weather = await response.json();
@@ -15,3 +16,11 @@ async function getWeather() {
 function fToCelcius(temp) {
   return (((temp - 32) * 5) / 9).toFixed(1);
 }
+
+//get value from search input
+const search = document.querySelector(".search");
+const city = document.querySelector("#city");
+
+search.addEventListener("click", () => {
+  getWeather(city.value);
+});
